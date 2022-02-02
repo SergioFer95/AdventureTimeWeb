cb65931a2a7626542a45a830a515c784982fa6eb
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-02-02 19:48:30 +0100

    Add routes and create navbar

--- AdventureTime/src/app/app-routing.module.ts
+++ AdventureTime/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ContactComponent } from './contact/contact.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'HomeComponent', pathMatch: 'full'},
+  {path:'Home', component: HomeComponent},
+  {path:'Personajes', component: CharactersComponent},
+  {path:'Contacto', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AdventureTime/src/app/app.module.ts
+++ AdventureTime/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './characters/characters.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './home/header/header.component';
+import { MainComponent } from './home/main/main.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharactersComponent,
+    ContactComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
